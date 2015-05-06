3db29be11afe4f283727c6bcb3386e7974e0c32b
SliceDaddy <rslipher@saintjoe.edu>
2015-05-06 17:07:36 -0400

    driver

--- FinalProjectRJS/src/FinalProjectRJS/Driver.java
+++ FinalProjectRJS/src/FinalProjectRJS/Driver.java
@@ -4,7 +4,19 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Mammal mammalOne, mammalTwo;
+		
+		mammalOne = new Mammal("Chimp",1);
+		mammalTwo = new Mammal("Whale",2);
+		
+		System.out.println("Data for mammal one: " + mammalOne.toString());
+		mammalOne.setMammalclass(1);
+		System.out.println("Data for mammal one: " + mammalOne.toString());
 
+		System.out.println("Data for mammal two: " + mammalTwo.toString());
+		mammalTwo.setMammalclass(2);
+		System.out.println("Data for mammal two: " + mammalTwo.toString());
 	}
 
 }
